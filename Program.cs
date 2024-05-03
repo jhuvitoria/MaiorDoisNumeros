7ff0f28b59635948009a6bd5ad3fb3c92381e8a6
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 20:18:53 -0300

    exercicio concluido!

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numero;
+
+Console.Write("digite o primeiro numero:");
+numero = Convert.ToInt32(Console.ReadLine());
+
+int numero2;
+
+Console.Write("digite o segundo numero:");
+numero2 = Convert.ToInt32(Console.ReadLine());
+
+if (numero > numero2)
+{
+    Console.WriteLine ($"{numero} é maior");
+
+}
+else 
+{
+    Console.WriteLine($"{numero2} é maior");
+}
+
